c8979f61ab63661b0aa3879edabd9415b27ea07f
Coraville <108678949+Coraville@users.noreply.github.com>
2024-05-08 10:51:40 -0300

    Create Geometrica.java

---
+++ Geometrica.java
@@ -0,0 +1,11 @@
+public class Geometrica implements ICalcMedia {
+    @Override
+    public double calculaMedia(double p1, double p2) {
+        return Math.sqrt(p1 * p2);
+    }
+
+    @Override
+    public String situacao(double media) {
+        return media > 5 ? "aprovado" : "reprovado";
+    }
+}
