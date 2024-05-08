38ff0d6b59342926c68270fabd08261ac6bc0d4c
Coraville <108678949+Coraville@users.noreply.github.com>
2024-05-08 10:54:37 -0300

    Update Geometrica.java

--- Geometrica.java
+++ Geometrica.java
@@ -6,6 +6,6 @@ public class Geometrica implements ICalcMedia {
 
     @Override
     public String situacao(double media) {
-        return media > 5 ? "aprovado" : "reprovado";
+        return media > 6 ? "aprovado" : "reprovado";
     }
 }
